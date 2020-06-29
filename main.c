4c47753f41585cf896ef42674b313e3f69e63a3c
CamiloCortes1753 <67564515+CamiloCortes1753@users.noreply.github.com>
2020-06-29 15:18:34 -0500

    Update main.c
    se añadió el ejercicio 3 y 4 y se corrigieron algunos errores de sintaxis tanto como añadir nuevas librerías

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
-#include <cstdbool>
 #include <math.h>
+#include <stdbool.h>
+#include<string.h>
+#include <stdlib.h>
+
+int Uw,Fll;
+char aux;
+
+int falling(char number[10]){
+    for (int i =0;i<strlen(number);i++){
+        for (int j =0;j<strlen(number)-1;j++){
+            if (number[j]>number[j+1]){
+                aux=number[j];
+                number[j]=number[j+1];
+                number[j+1]=aux;
+            }
+        }
+    }
+    Fll=atoi(number);
+    return Fll;
+}
+int Upward(char number[10]){
+    for (int i =0;i<strlen(number);i++){
+        for (int j =0;j<strlen(number)-1;j++){
+            if (number[j]<number[j+1]){
+                aux=number[j];
+                number[j]=number[j+1];
+                number[j+1]=aux;
+            }
+        }
+    }
+    Uw=atoi(number);
+    return Uw;
+}
+int numberMagic(char number[10]) {
+    return Upward(number)-falling(number);
+}
+
 void numPrimos(int num){
     int cont;
     int numPrim;
@@ -81,31 +117,36 @@ int prueba() {
 
 }
 
-int main() {
-    int opt=0;
-    bool continuar=true;
+double CalImc(float peso, float altura) {
+    return peso/(altura*altura);
+}
 
-    while (continuar){
+
+int main() {
+    int opt = 0;
+    bool continuar = true;
+
+    while (continuar) {
 
         printf("\n\n\t-*-*-*-*-*-*-* BIENVENIDOS -*-*-*-*-*-*-*-*-*\n\n");
 
         printf("1) Encontrar los numeros primos descendentemente. \n");
         printf("2) Verificar si el numero es egolatra o no.\n");
-        printf("3) ...........................\n");
-        printf("4)...................\n");
+        printf("3) Numero Magico\n");
+        printf("4) IMC \n");
         printf("5).............\n");
         printf("6)....................\n");
         printf("7) Ver el resultado de la prueba ya registrada. \n");
         printf("8) Salir del programa \n");
         printf("Que desea hacer? \n");
-        scanf("%d",&opt);
+        scanf("%d", &opt);
 
-        switch (opt){
+        switch (opt) {
 
             case 1:
-                int numPrim;
                 printf("\tEjercicio 1\n");
                 printf("Digite un numero\n");
+                int numPrim;
                 scanf("%d", &numPrim);
                 if (numPrim > 0) {
                     numPrimos(numPrim);
@@ -115,54 +156,120 @@ int main() {
                 break;
 
             case 2:
-                double num;
                 printf("\tEjercicio 2 \n");
                 printf("Digite un numero para saber si es egolatra o no \n");
-                scanf("%d",&num);
+                double num;
+                scanf("%d", &num);
                 numEgolatra(num);
 
                 break;
 
             case 3:
-                printf("\tEjercicio 3 \n");
 
+
+                printf("Bienvenido al Programa de Numeros Magicos \n");
+                printf("Digite un numero: \n");
+                char number[10];
+                scanf("%s", number);
+                int nb;
+                nb = atoi(number);
+                if (nb == numberMagic(number)) {
+                    printf("El numero %i es un numero magico\n",nb);
+                }
+                else {
+                    printf("El numero %i no es un numero magico\n",nb);
+                }
                 break;
 
             case 4:
-                printf("\tEjercicio 4 \n");
+                printf("¿Es usted Hombre o Mujer?\n");
+                printf("Escribe h para hombre y m para mujer\n");
+                char genero;
+                scanf("%s", &genero);
+                printf("Digite su peso?\n");
+                float peso,altura;
+                if (scanf("%f", &peso) != 1) {
+                    printf("solo se puede ingresar numeros");
+                } else {
+                    printf("Digite su altura \n");
+                    if (scanf("%f", &altura) != 1) {
+                        printf("solo se puede ingresar numeros");
+                    } else {
+                        switch (genero) {
+                            case 'h':
+                                if (CalImc(peso, altura) <= 17) {
+                                    printf("Desnutricion");
+                                } else if (CalImc(peso, altura) > 17 && CalImc(peso, altura) < 20) {
+                                    printf("Bajo peso");
+                                } else if (CalImc(peso, altura) >= 20 && CalImc(peso, altura) < 25) {
+                                    printf("Normal");
+                                } else if (CalImc(peso, altura) >= 25 && CalImc(peso, altura) < 30) {
+                                    printf("Sobrepeso");
+                                } else if (CalImc(peso, altura) >= 30 && CalImc(peso, altura) < 35) {
+                                    printf("Obesidad");
+                                } else if (CalImc(peso, altura) >= 35 && CalImc(peso, altura) <= 40) {
+                                    printf("Obesidad Marcada");
+                                } else if (CalImc(peso, altura) > 40) {
+                                    printf("Obesidad Morbida ");
+                                }
+                                break;
+                            case 'm':
+                                if (CalImc(peso, altura) <= 16) {
+                                    printf("Desnutricion");
+                                } else if (CalImc(peso, altura) > 16 && CalImc(peso, altura) < 20) {
+                                    printf("Bajo peso");
+                                } else if (CalImc(peso, altura) >= 20 && CalImc(peso, altura) < 24) {
+                                    printf("Normal");
+                                } else if (CalImc(peso, altura) >= 24 && CalImc(peso, altura) < 29) {
+                                    printf("Sobrepeso");
+                                } else if (CalImc(peso, altura) >= 29 && CalImc(peso, altura) < 34) {
+                                    printf("Obesidad");
+                                } else if (CalImc(peso, altura) >= 34 && CalImc(peso, altura) <= 39) {
+                                    printf("Obesidad Marcada");
+                                } else if (CalImc(peso, altura) > 39) {
+                                    printf("Obesidad Morbida ");
+                                }
+                                break;
+                            default:
+                                printf("Error %s no es valido por favor ingresa de nuevo", genero);
+                                break;
+                        }
+                    }
+                    break;
 
-                break;
+                    case 5:
+                        printf("\tEjercicio 5 \n");
 
-            case 5:
-                printf("\tEjercicio 5 \n");
+                    break;
 
-                break;
+                    case 6:
+                        printf("\tEjercicio 6\n ");
 
-            case 6:
-                printf("\tEjercicio 6\n ");
+                    break;
 
-                break;
+                    case 7:
 
-            case 7:
-
-                printf("\tEjercicio 7\n ");
-                printf("\nla suma dio! %d \n",prueba());
+                        printf("\tEjercicio 7\n ");
+                    printf("\nla suma dio! %d \n", prueba());
 
 
-                break;
+                    break;
 
-            case 8:
-                printf("\tPrograma terminado que tenga buen dia \n");
-                continuar=false;
+                    case 8:
+                        printf("\tPrograma terminado que tenga buen dia \n");
+                    continuar = false;
 
 
-                break;
-            default: puts("Error");
-                continuar=false;
+                    break;
+                    default:
+                        puts("Error");
+                    continuar = false;
+
+                }
 
         }
 
+        return 0;
     }
-
-    return 0;
 }
+
